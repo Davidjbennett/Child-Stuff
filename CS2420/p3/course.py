74ceea37bfdac6d7c24c912daab8a3ad65988d19
Davidjbennett <davyj.bennett@gmail.com>
2021-03-01 12:28:13 -0700

    CS2420 p3 and web dev stuff

--- CS2420/p3/course.py
+++ CS2420/p3/course.py
@@ -4,14 +4,16 @@ class Course:
     """Course initializer which gives course its various info"""
     def __init__(self, course_num:int = 0, course_name:str = '',
                 course_credits:float = 0.0, gpa:float = 0.0):
-        self.course_num = course_num
+        self.course_num = int(course_num)
         self.course_name = course_name
-        self.course_credits = course_credits
-        self.gpa = gpa
+        self.course_credits = float(course_credits)
+        self.gpa = float(gpa)
+        self.next = None
+        self.prev = None
 
     def number(self):
         """returns course number"""
-        return self.course_num
+        return int(self.course_num)
 
     def name(self):
         """returns course name"""
@@ -27,5 +29,5 @@ class Course:
 
     def __str__(self):
         """returns toString of course with number, name, credits, and gpa"""
-        return (f"CS{self.course_num} {self.course_name} Grade:{self.gpa}"+
-        f"Credit Hours: {self.course_credits:.1f}")
+        return ("CS"+str(self.course_num)+" "+self.course_name+" Grade:"+
+                str(self.gpa)+" Credit Hours: "+str(self.course_credits))
--- CS2420/p3/courselist.py
+++ CS2420/p3/courselist.py
@@ -1,19 +1,17 @@
 """Pointer and Courselist are datastructures used to hold data. It is a
 Ordered Linked List"""
-class Pointer:
-    """Pointer holds data and points to the next and previous data pieces"""
-    def __init__(self, dataval = None):
-        self.dataval = dataval
-        self.next = None
-        self.prev = None
-
-
+# class Pointer:
+#     """Pointer holds data and points to the next and previous data pieces"""
+#     def __init__(self, dataval = None):
+#         self.dataval = dataval
+#         self.next = None
+#         self.prev = None
 class CourseList:
     """Courselist is a ordered linked list type used to hold courses"""
     
     def __init__(self):
-        self.first = None
-        self.last = None
+        self.head = None
+        self.tail = None
         self.count = 0
     # A method that i was using
     # def info(self):
@@ -22,46 +20,47 @@ class CourseList:
 
     def insert(self, dataval):
         """Inserts a course into the courselist in order of class number"""
-        point = Pointer(dataval)
-        if self.first is None:
-            self.first = point
-            self.last = self.first
-        elif self.first.dataval.number() >= dataval.number():
-            self.first = self.__set_links(None, point, self.first)
-        elif self.last.dataval.number() <= dataval.number():
-            self.last = self.__set_links(self.last, point, None)
+        # point = Pointer(dataval)
+        if self.head is None:
+            self.head = dataval
+            self.tail = self.head
+        elif self.head.course_num >= dataval.course_num:
+            self.head = self.__set_links(None, dataval, self.head)
+        elif self.tail.course_num <= dataval.course_num:
+            self.tail = self.__set_links(self.tail, dataval, None)
         else:
-            current = self.first
-            while current.dataval.number() < point.dataval.number():
+            current = self.head
+            while current.course_num < dataval.course_num:
                 current = current.next
-            self.__set_links(current.prev, point, current)
+            self.__set_links(current.prev, dataval, current)
         self.count += 1
+        current=self.head
 
     def remove(self, number):
         """Removes a course according to number parameter"""
-        current = self.first
-        if number >= self.first.dataval.number() and number <= self.last.dataval.number():
-            while current.next is not None and current.dataval.number() is not number:
+        current = self.head
+        if number >= self.head.course_num and number <= self.tail.course_num:
+            while current.next != None and current.course_num != number:
                 current = current.next
-            if current.dataval.number() == number:
+            if current.course_num == number:
                 self.__remove_item(current)
 
     def remove_all(self, number):
         """Removes all courses with specified class number"""
-        current = self.first
-        while current.next is not None:
-            if current.dataval.number() == number:
+        current = self.head
+        while current.next != None:
+            if current.course_num == number:
                 self.__remove_item(current)
             current = current.next
 
     def find(self, number):
         """finds first occurence of number in courselist"""
-        current = self.first
-        if number >= self.first.dataval.number() and number <= self.last.dataval.number():
-            while current.next is not None and current.dataval.number() is not number:
+        current = self.head
+        if number >= self.head.course_num and number <= self.tail.course_num:
+            while current.next != None and current.course_num != number:
                 current = current.next
-            if current.dataval.number() == number:
-                return current.dataval
+            if current.course_num == number:
+                return current
         return -1
 
     def size(self):
@@ -70,21 +69,21 @@ class CourseList:
 
     def calculate_gpa(self):
         """Calculates and returns gpa of courses in courselist"""
-        if self.first is not None:
-            current = self.first
+        if self.head != None:
+            current = self.head
             total_grade_points = 0.0
             total_credits = 0.0
-            while current.next is not None:
-                total_grade_points += current.dataval.grade() * current.dataval.credit_hr()
-                total_credits +=  current.dataval.credit_hr()
+            while current.next != None:
+                total_grade_points += current.gpa * current.course_credits
+                total_credits +=  current.course_credits
                 current = current.next
             return total_grade_points/total_credits
 
     def is_sorted(self):
         """returns true if list is sorted and false otherwise"""
-        current = self.first
+        current = self.head
         while current.next is not None:
-            if current.dataval.number() > current.next.dataval.number():
+            if current.course_num > current.next.course_num:
                 return False
             current = current.next
         return True
@@ -92,30 +91,30 @@ class CourseList:
     def __str__(self):
         """returns list as a string"""
         string = ""
-        current = self.first
+        current = self.head
         while current is not None:
-            string += str(current.dataval) + "\n"
+            string += str(current) + "\n"
             current = current.next
         return string
 
     def __iter__(self):
         """iterates through courselist items"""
-        current = self.first
-        while current is not None:
+        current = self.head
+        while current != None:
             yield current
             current = current.next
 
     def __set_links(self, before, item, after):
         """Helper method that sets new links when courselist is changed"""
-        if item is None:
-            if before is not None:
+        if item == None:
+            if before != None:
                 before.next = after
-            if after is not None:
+            if after != None:
                 after.prev = before
         else:
-            if before is not None:
+            if before != None:
                 before.next = item
-            if after is not None:
+            if after != None:
                 after.prev = item
             item.prev = before
             item.next = after
@@ -123,9 +122,9 @@ class CourseList:
 
     def __remove_item(self, current):
         """Helper function that removes an item and sets new links in courselist"""
-        if current is self.first:
-            self.first = current.next
-        if current is self.last:
-            self.last = current.prev
+        if current == self.head:
+            self.head = current.next
+        if current == self.tail:
+            self.tail = current.prev
         self.__set_links(current.prev, None, current.next)
         self.count -= 1
