7c41b4cca5f643760c256629518155552f444a1e
Davidjbennett <davyj.bennett@gmail.com>
2021-07-08 16:48:48 -0600

    Stats Proj update

--- Stats/HouseMarket.py
+++ Stats/HouseMarket.py
@@ -1,3 +1,17 @@
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+df = pd.read_csv('UTSTHPI.csv')
 
+# df.info()
+# print(df.head())
+print(df['DATE'].value_counts().head())
+
+#!Get data from csv
+#!Display data on a graph
+#!Put data into a table
+#!Take table and calculate next 10 years of possible house cost
+#!display new data on a table
+#!Include descriptions on the graphs and data 
